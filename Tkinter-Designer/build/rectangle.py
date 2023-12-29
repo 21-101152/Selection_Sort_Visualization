c7a15f51595a05be2ee8bddf135f8062eccca62a
Amar <21-101152@students.eui.edu.eg>
2023-12-30 00:45:25 +0200

    Rectangle class

--- Tkinter-Designer/build/rectangle.py
+++ Tkinter-Designer/build/rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     xright = 90
     ydown = 289
     yup = 329
+    index = 1
     def __init__ (self, canvas, fill_color, outline_color, text):
         self.canvas = canvas
         self.text = text
@@ -13,6 +14,7 @@ class Rectangle:
         self.fill_color = fill_color
         self.outline_color = outline_color
         self.id = self.create_rectangle()
+        self.index += 1
         Rectangle.xleft += 40
         Rectangle.xright += 40
     
@@ -25,18 +27,15 @@ class Rectangle:
         fill=self.fill_color,
         outline=self.outline_color
         )
-        self.canvas.create_text(
+        self.text_widget = self.create_text()
+        return rec
+    
+    def create_text(self):
+        txt = self.canvas.create_text(
         self.xleft + 20,
         309,
         text = self.text,
         font=("Jost Bold", 12, "bold")
         )
-        return rec
-    
-    def update_rectangle_color(self, new_color):
-        self.canvas.itemconfig(self, fill = new_color)
+        return txt
 
-    def canvas_get(self):
-        return self.canvas
-        
-        
